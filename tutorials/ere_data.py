c696595b7f56a00496ecbdf10b960ee511f925f2
fstrnad <felix.strnad@uni-tuebingen.de>
2023-04-18 16:44:36 +0200

    UPdated tutorial on create ERE data.

--- tutorials/ere_data.py
+++ tutorials/ere_data.py
@@ -13,29 +13,35 @@ output_dir = "/home/strnad/data/"
 plot_dir = "/home/strnad/data/climnet/plots/"
 
 dataset_file = output_dir + \
-    f"/climate_data/mswep_pr_{1}_ds.nc"
+    f"/climate_data/mswep_pr_{1}_1979_2021_ds.nc"
 grid_step = 1
 ds_pr_mswep = bds.BaseDataset(data_nc=dataset_file,
-                              can=False,
                               grid_step=grid_step,
                               lon_range=[40, 150],
                               lat_range=[-25, 50]
                               )
 
 # %%
+# Create Event time series
 ds_pr_mswep.create_evs_ds(var_name='pr',
                           q=0.9)
 # %%
+# Save results as nc file
 savepath =  output_dir + \
     f"/evs_data/mswep_pr_{grid_step}_evs.nc"
 ds_pr_mswep.save(filepath=savepath,
-                 zlib=True)  # zlib for data compression
+                 zlib=False)  # zlib for data compression
+
 # %%
+# Test if file is stored correctly and read file
+ds_pr_evs = bds.BaseDataset(data_nc=savepath,
+                              )
+
 # %%
 # Plot together EEs and Q-values
 reload(cplt)
-num_eres = ds_pr_mswep.ds.evs.sum(dim='time')
-mean_pr = ds_pr_mswep.ds.pr.mean(dim='time')
+num_eres = ds_pr_evs.ds.evs.sum(dim='time')
+mean_pr = ds_pr_evs.ds.pr.mean(dim='time')
 fdic = cplt.create_multi_plot(
     1, 2,
     figsize=(12, 5),
