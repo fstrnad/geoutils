a562a8644ff88939ef4859af266fdfd1c3824bc6
fstrnad <felix.strnad@uni-tuebingen.de>
2025-08-14 11:06:05 +0200

    updated plot sizes

--- geoutils/plotting/plotting_utils.py
+++ geoutils/plotting/plotting_utils.py
@@ -183,7 +183,7 @@ def set_title(title, ax=None, fig=None, **kwargs):
                          fontweight=fw,
                          fontsize=fsize)
     if vertical_title is not None:
-        fsize = kwargs.pop('vertical_title_fsize', pst.BIGGER_SIZE)
+        fsize = kwargs.pop('vertical_title_fsize', pst.MEDIUM_SIZE)
         ax.text(x=x_title_offset, y=.5, s=vertical_title,
                 transform=ax.transAxes,
                 color=title_color, rotation='vertical',
